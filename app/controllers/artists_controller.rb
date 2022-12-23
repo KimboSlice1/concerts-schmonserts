0eebb4e3b7b7d484807ff41ef9a001adbfa06134
Caleb McCoy <caleb_mccoy115@yahoo.com>
2022-12-23 11:20:46 -0600

    artist can show my id

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,6 +2,15 @@ class ArtistsController < ApplicationController
     def index
         render json: Artist.all
     end
+    
+    def show
+        artist_found = Artist.find_by_id(params[:id])
+        if artist_found
+            render json: artist_found #add serializer here if we need to show an additional model
+        else
+            render json: {"error": "Aritst not found"}, status: :not_found
+        end
+    end
 
     def create
         new_artist = Artist.create(new_artist_params)
