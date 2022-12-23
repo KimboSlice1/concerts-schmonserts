af3ec63e547c14d9fc60427932ad1f16fcbe03cd
Nick Phelps <nickphelps34@gmail.com>
2022-12-23 11:47:59 -0500

    Seeding worked on

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,39 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Artist.destroy_all
 Concert.destroy_all
+User.destroy_all
+
+u1 = User.create(username: "Nickphelps34", password: 9362, email: "Nickphelps34@gmail.com", bio: “”)
+u2 = User.create(username: "kh582013", password: 9362, email: "kh582013@gmail.com", bio: “”)
+u3 = User.create(username: "cmccoy115", password: 9362, email: "caleb_mccoy115@yahoo.com", bio: “”)
+
+
+a1 = Artist.create(name: "Subtronics", genre: "Electronic", album: "Fractals")
+a2 = Artist.create(name: "Svdden Death", genre: "Electronic", album: "Voyd 2")
+a3 = Artist.create(name: "Bring Me The Horizon", genre: "Rock" , album: "Thats the Spirit")
+a4 = Artist.create(name: "EDEN", genre: "Indie" , album: "I Think You Think Too Much of Me" )
+a5 = Artist.create(name: "blackbear", genre: "Pop" , album: "Digital Druglord")
+a6 = Artist.create(name: "Spooky Black", genre: "Alternative", album: "Leaving")
+a7 = Artist.create(name: "Machine Gun Kelly", genre: "Alternative" , album: "Tickets to my downfall" )
+a8 = Artist.create(name: "Taylor Swift" , genre: "Country" , album: "Folklore" )
+a9 = Artist.create(name: "Tyedye Ky" , genre: "Electronic", album: "Baby Blue and the Super Moon")
+a10 = Artist.create(name: "Vacations", genre: "Alternative" , album: "Vibes & Days" )
+a11 =  Artist.create(name: "Metallica" , genre: "Metal" , album: "The Black Album" )
+a12 = Artist.create(name: "Deadmau5" , genre: "Electronic" , album: "4x4=12" )
+a13 = Artist.create(name: "Skrillex", genre: "Electronic" , album: "Recess")
+a14 = Artist.create(name: "Adam Tell" , genre: "Electronic" , album: "Recomposure" )
+a15 = Artist.create(name: "Phish", genre: "Jam Band" , album: "Round Room")
+a16 = Artist.create(name: "Sleeping With Sirens", genre: "Metalcore", album: "Complete Collapse" )
+a17 = Artist.create(name: "Eptic", genre: "Electronic" , album: "The End of the World" )
+a18 = Artist.create(name: "Imanu" , genre: "Electronic" , album: "Unfold" )
+a19 = Artist.create(name: "EAZYBAKED" , genre: "Electronic" , album: "INTERTWINED" )
+a20 = Artist.create(name: "Jeremy Zucker", genre: "Pop" , album: "Motions" )
+
+Concert.create(artist_id: a5.id, user_id: u2.id, city: "Columbus", date: 02152023 )
+Concert.create(artist_id: a8.id, user_id: u2.id, city: "Columbus", date: 03272023)
+Concert.create(artist_id: a13.id, user_id: u1.id, city: "Grand Rapids", date: 06232023)
+Concert.create(artist_id: a18.id, user_id: u1.id, city: "Grand Rapids", date: 02152023)
+Concert.create(artist_id: a4.id, user_id: u3.id, city: "Nashville", date: 02022023)
+Concert.create(artist_id: a14.id, user_id: u3.id, city: "Nashville", date: 02242023)
 
 
