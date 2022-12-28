a115df241b6e130729608bea74c2c6b82001b3a0
Kimber Herron <kh582013@gmail.com>
2022-12-28 07:59:38 -0500

    fetch to home page finished, h1 added to verify

--- javascript/src/App.js
+++ javascript/src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
+
+const concertsSchmonsertsURL="http://localhost:4000/"
 
 function App() {
+  fetch( concertsSchmonsertsURL )
+  .then( r => r.json () )
+  .then(console.log())
+
+
+
   return (
+
+    
     <div className="App">
-     
+     <h1>This is the home page motherfuckers</h1>
     </div>
   );
 }
