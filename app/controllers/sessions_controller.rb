65fc42b82d65070384b75777bd530927e15b2cd7
Caleb McCoy <caleb_mccoy115@yahoo.com>
2022-12-29 18:43:07 -0600

    added a session login method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
+
+  def login
+    user = User.find_by(username:params[:username])
+
+    if user&.authenticate(params[:password])
+
+      render json: user, status: :ok
+    else 
+      render json: {errors: 'Invalid Password or Username'}, status: :unauthorized
+    end
+  end
   
   def index
     session[:session_hello] ||= "World"
