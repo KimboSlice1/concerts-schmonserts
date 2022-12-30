376b679aaf079b5510e9ec867478ebd05e9c09f8
Caleb McCoy <caleb_mccoy115@yahoo.com>
2022-12-29 18:32:44 -0600

    added the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find(params[:id])
+        render json: user, status: :ok
+    end
+
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+
+    private
+
+    def user_params
+        params.permit(:username, :email, :bio, :password)
+    end
 end
---
+++ javascript/src/ConcertForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {useState} from 'react';
+
+const ConcertForm = ({}) => {
+    const [newConcertObj, setNewConcertObj] = useState(
+        {
+            
+        }
+    )
+
+return(
+    <div>
+        
+    </div>
+)
+}
