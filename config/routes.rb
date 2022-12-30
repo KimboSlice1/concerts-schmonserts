074ba3941bc6de9947ee2b6ad1657f61e8825d2e
Kimber Herron <kh582013@gmail.com>
2022-12-30 07:40:24 -0500

    added concern updated sessions controller but commented out new info until approved. application controller updated. routes updated accordiningly

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   resources :concerts
   resources :artists
   resources :users
+  resources :sessions, only: [:create]
+  # update later when adding delete and shit
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
+  resources :registrations, only: [:create]
   get "/sessions", to: "sessions#index"
+root to: "static#home"
 end
