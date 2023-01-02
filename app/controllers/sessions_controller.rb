227b004242eda2e830f7bf56d77262712f5826e3
Kimber Herron <kh582013@gmail.com>
2023-01-02 07:50:53 -0500

    final touches on backend user auth... I think

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   # def create
   #   user = User
   #   .find_by(email: params["user"]["email"])
+  #^^ email is passed into user as a nested value
   #   .try(:authenticate, params["user"]["password"])
+  #^^kinda the same
 
   #   if user 
   #     session[:user_id]=user.id
