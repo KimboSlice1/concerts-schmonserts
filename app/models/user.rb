5245fffb8f4049d17b38f248c66b2ef579d9d1fa
Kimber Herron <kh582013@gmail.com>
2023-01-02 07:18:12 -0500

    added validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :concerts
   has_many :artists, through: :concerts
   has_secure_password
+
+  validates :email, :bio, :username, :password, presence: true
+
+
 end
