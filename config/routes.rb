62fa6ec874ab68675e15b4c1fa8404e69605aa2b
Kimber Herron <kh582013@gmail.com>
2023-01-05 10:51:24 -0500

    updated sessions controller and loginregistration

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create]
   resources :addartists, only: [ :show ]
 #   update later when adding delete and shit
+
+
+post '/login', to:'sessions#create'
+post '/login', to:'sessions#login'
 #   delete :logout, to: "sessions#logout"
 #   get :logged_in, to: "sessions#logged_in"
 #   resources :registrations, only: [:create]
--- javascript/src/Components/LoginRegistration.js
+++ javascript/src/Components/LoginRegistration.js
@@ -1,6 +1,6 @@
 import{useState} from 'react';
 
-const userLoginUrl='/user'
+const userLoginUrl='/login'
 
 function LoginRegistration(){
 
